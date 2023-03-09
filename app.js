3744925e4a0900c47f6f9776aba699a2f299bcb6
owentashlin <owentashlin@gmail.com>
2023-03-08 17:10:56 -0800

    start function working

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.querySelector('#score')
     const startBtn = document.querySelector('#start_button')
     const width = 10
+    let nextRandom = 0
+    let timerId
 
 //Tetriminoes
 const lTetromino = [
@@ -62,7 +64,7 @@ const lTetromino = [
     })
   }
 
-  timerId = setInterval(moveDown, 1000)
+  //timerId = setInterval(moveDown, 1000)
 
   function control(e) {
     if(e.keyCode === 37){
@@ -87,10 +89,12 @@ const lTetromino = [
   function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-        random = Math.floor(Math.random()*tetriminoes.length)
+        random = nextRandom
+        nextRandom = Math.floor(Math.random()*tetriminoes.length)
         current = tetriminoes[random][currentRotation]
         currentPosition = 4
         draw()
+        displayShape()
     }
   }
 
@@ -128,12 +132,48 @@ const displaySquares = document.querySelectorAll('.mini-grid div')
 const displayWidth = 4
 let displayIndex = 0
 
+
 const upNextTetrimino = [
-    [1, width+1, width*2+1, 2],
-    [0,width,width+1,width*2+1],
-    [1,width,width+1,width+2],
-    [0,1,width,width+1],
-    [1,width+1,width*2+1,width*3+1],
+    [1, displayWidth+1, displayWidth*2+1, 2],
+    [0,displayWidth,displayWidth+1,displayWidth*2+1],
+    [1,displayWidth,displayWidth+1,displayWidth+2],
+    [0,1,displayWidth,displayWidth+1],
+    [1,displayWidth+1,displayWidth*2+1,displayWidth*3+1],
 ]
  
+function displayShape() {
+    displaySquares.forEach(square => {
+        square.classList.remove('tetrimino')
+    })
+    upNextTetrimino[nextRandom].forEach(index => {
+        displaySquares[displayIndex + index].classList.add('tetrimino')
+    })
+}
+
+startBtn.addEventListener('click', () => {
+    if (timerId) {
+        clearInterval(timerId)
+        timerId = null
+    } else {
+        draw()
+        timerId = setInterval(moveDown, 1000)
+        nextRandom = Math.floor(Math.random()*tetriminoes.length)
+        displayShape()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
